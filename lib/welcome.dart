d81422676834e3824f5573c76fd7d444ee3a3505
kgovind12 <kgovind@ucdavis.edu>
2020-08-09 15:29:45 -0700

    initial commit 1

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class MyApp extends StatefulWidget {
-  SecondPage createState() => SecondPage();
+  WelcomePage createState() => WelcomePage();
 }
 
-class SecondPage extends State<MyApp> {
+class WelcomePage extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,8 +17,18 @@ class SecondPage extends State<MyApp> {
         body: Column(
           children: <Widget>[
             Text('Welcome to Activitree'),
-            FlatButton(onPressed: null, child: Text('Login')),
-            FlatButton(onPressed: null, child: Text('Sign up'))
+            FlatButton(
+                onPressed: () {
+                  print('login pressed');
+                },
+                child: Text('Login'),
+                color: Colors.teal[300]),
+            FlatButton(
+                onPressed: () {
+                  print('Sign-up pressed');
+                },
+                child: Text('Sign up'),
+                color: Colors.teal[300])
           ],
         ),
       ),
